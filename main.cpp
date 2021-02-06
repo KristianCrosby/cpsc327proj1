fb585745b99228668d8767d830dfcc19d0b1855f
KristianCrosby <kristian.crosby.18@cnu.edu>
2021-02-06 14:04:31 -0500

    finished

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//#includes are ALWAYS at top of file
+#include "myfunc.h"	//iostream is where std::cout and std::endl are defined
+#include <iostream>
+
+int main()
+{
+	std::cout<<func()<<std::endl;
+	return 0;
+}
